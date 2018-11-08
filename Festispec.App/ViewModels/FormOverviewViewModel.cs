4abeca23f5cc898ab1f2ff772bcb53710c53745a
Aaron Slots <85274163+AaronSlots@users.noreply.github.com>
2018-11-08 15:47:21 +0100

    Startup

---
+++ Festispec.App/ViewModels/FormOverviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Festispec.Database.Models;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec.App.ViewModels
+{
+	public class FormOverviewViewModel:ViewModelBase
+	{
+		public ObservableCollection<Form> Forms{
+			}
+	}
+}
