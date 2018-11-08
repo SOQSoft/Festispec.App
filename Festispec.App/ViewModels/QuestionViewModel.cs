755a90a302b15d34e061ddcc276547d37120ff66
TheXianchu <dquillet@avans.nl>
2018-11-08 18:09:33 +0100

    Base commit for ViewModels Question and QuestionItem

--- Festispec.App/ViewModels/QuestionViewModel.cs
+++ Festispec.App/ViewModels/QuestionViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;ge
 using Festispec.App.Annotations;
 using Festispec.Database.Models;
 using GalaSoft.MvvmLight;
@@ -13,14 +14,52 @@ namespace Festispec.App.ViewModels
 {
     public class QuestionViewModel : ViewModelBase
     {
-        private readonly Question Question;
+        public int Id
+        {
+            get => _question.Id;
+            set
+            {
+                _question.Id = value;
+                base.RaisePropertyChanged();
+            }
+        }
+        public string Text
+        {
+            get => _question.Text;
+            set
+            {
+                _question.Text = value;
+                base.RaisePropertyChanged();
+            }
+        }
+        public string Description
+        {
+            get => _question.Description;
+            set
+            {
+                _question.Description = value;
+                base.RaisePropertyChanged();
+            }
+        }
+        public int QuestionType
+        {
+            get => _question.QuestionType;
+            set
+            {
+                _question.QuestionType = value;
+                base.RaisePropertyChanged();
+            }
+        }
+        public ObservableCollection<QuestionItemViewModel> QuestionItems { get; set; }
 
+        private readonly Question _question;
+        public event PropertyChangedEventHandler PropertyChanged;
         public QuestionViewModel(Question question)
         {
-            Question = question;
-        }
+            _question = question;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+            QuestionItems = new ObservableCollection<QuestionItemViewModel>(question.QuestionItem.Select(o => new QuestionItemViewModel(o)));
+        }
 
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -30,7 +69,7 @@ namespace Festispec.App.ViewModels
 
         public Question ToModel()
         {
-            return Question;
+            return _question;
         }
     }
 }
