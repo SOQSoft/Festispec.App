3f52254ee297ed2ce5cdc7641215fa31b83de378
sjesmuld <31733294+sjesmuld@users.noreply.github.com>
2018-11-09 12:48:50 +0100

    schermen gemaakt

---
+++ Festispec.App/Views/CreateQuestion.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Festispec.App.Views
+{
+    /// <summary>
+    /// Interaction logic for CreateQuestion.xaml
+    /// </summary>
+    public partial class CreateQuestion : Page
+    {
+        private int count;
+        public CreateQuestion()
+        {
+            InitializeComponent();
+        }
+
+        public static T TrycloneElement<T>(T orig)
+        {
+            try
+            {
+                string s = XamlWriter.Save(orig);
+
+                StringReader stringReader = new StringReader(s);
+
+                XmlReader xmlReader = XmlTextReader.Create(stringReader, new XmlReaderSettings());
+                XmlReaderSettings sx = new XmlReaderSettings();
+
+                object x = XamlReader.Load(xmlReader);
+                return (T)x;
+            }
+            catch
+            {
+                return (T)((object)null);
+            }
+
+        }
+
+        private void addQuestion(object sender, RoutedEventArgs e)
+        {
+            TabItem tab2 = TrycloneElement(defaultQuestion);
+
+            if (tab2 != null)
+            {
+                tab2.Visibility = Visibility.Visible;
+                main.Items.Add(tab2);
+            }
+
+        }
+    }
+}
