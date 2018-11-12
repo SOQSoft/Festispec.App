d95bef74228a458fc88ae74fb9b60d2589ed0e12
Stan <sjesmuld@avans.nl>
2018-11-12 20:03:44 +0100

    1e implementatie menu functionaliteit gemaakt

--- Festispec.App/ViewModels/MainViewModel.cs
+++ Festispec.App/ViewModels/MainViewModel.cs
@@ -1,4 +1,9 @@
+using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace Festispec.App.ViewModels
 {
@@ -14,12 +19,29 @@ namespace Festispec.App.ViewModels
     /// See http://www.galasoft.ch/mvvm
     /// </para>
     /// </summary>
-    public class MainViewModel : ViewModelBase
+    public class MainViewModel : ViewModelBase, INotifyPropertyChanged
     {
-        
+        public ICommand TemplatePageBtn { get; set; }
+        public ICommand AddQuestion { get; set; }
+        public Uri DisplayPage { get; set; }
+
         public MainViewModel()
         {
-        
+            TemplatePageBtn = new RelayCommand(ShowTemplatePage);
+            AddQuestion = new RelayCommand(ViewCreateQuestion);
+            DisplayPage = new Uri("QuestionListHome.xaml", UriKind.Relative);
+        }
+
+        public void ShowTemplatePage()
+        {
+            DisplayPage = new Uri("TemplateList.xaml", UriKind.Relative);
+            RaisePropertyChanged("DisplayPage");
+        }
+
+        public void ViewCreateQuestion()
+        {
+            DisplayPage = new Uri("CreateQuestion.xaml", UriKind.Relative);
+            RaisePropertyChanged("DisplayPage");
         }
     }
 }
