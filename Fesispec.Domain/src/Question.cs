3473f31db2794140fbac188fd13b244c3dd13126
Guus van der Meer <guus.meer@outlook.com>
2018-11-14 13:37:26 +0100

    Fixed repo

---
+++ Fesispec.Domain/src/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Festispec.Domain.src;
+
+namespace Festispec.Database.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public QuestionType QuestionType { get; set; }
+        public int? FormId { get; set; }
+        public Form Form { get; set; }
+        public ICollection<QuestionItem> QuestionItem { get; set; } = new List<QuestionItem>();
+    }
+}
