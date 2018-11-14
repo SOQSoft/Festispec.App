b79e4d0e42a1a3a33e898a1aebf8f31fb2dc526a
sheertum <simonvanheertum@gmail.com>
2018-11-14 16:30:10 +0100

    Created User repo interface Created TestRepo

---
+++ Festispec.App/Repositories/IUsersRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Festispec.Domain;
+
+namespace Festispec.App.Repositories
+{
+    public interface IUsersRepository
+    {
+        void Add(User user);
+        void Delete(User user);
+        void Update(User user);
+        ICollection<User> GetAll();
+    }
+}
---
+++ Festispec.App/Repositories/UserTestRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Festispec.Domain;
+
+namespace Festispec.App.Repositories
+{
+    class UserTestRepository : IUsersRepository
+    {
+        private static List<User> Users { get; set; } = new List<User>();
+        public UserTestRepository()
+        {
+            InitialiseDate();
+        }
+
+        public void InitialiseDate()
+        {
+            User user = new User();
+            user.Password = "test";
+            user.Username = "test1";
+            user.Role = new Role();
+            user.Employee = new Employee();
+            Users.Add(user);
+
+            user = new User();
+            user.Password = "abcd";
+            user.Username = "test2";
+            user.Role = new Role();
+            user.Employee = new Employee();
+            Users.Add(user);
+
+            user = new User();
+            user.Password = "welkom1";
+            user.Username = "test3";
+            user.Role = new Role();
+            user.Employee = new Employee();
+            Users.Add(user);
+        }
+        public void Add(User user)
+        {
+            Users.Add(user);
+        }
+
+        public void Delete(User user)
+        {
+            Users.Remove(user);
+        }
+
+        public ICollection<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User user)
+        {
+            var u = Users.FirstOrDefault(o => o.Username == user.Username);
+            u = user;
+        }
+    }
+}
