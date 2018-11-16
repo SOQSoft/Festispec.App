715a21aa754506655c1fd1ad8f4a3b65d194f403
Bart <bahfheuv.avans@gmail.com>
2018-11-16 11:24:54 +0100

    Role Repo

---
+++ Festispec.App/Repositories/RoleRepository.cs
@@ -0,0 +1,33 @@
+﻿using Festispec.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec.App.Repositories
+{
+    class RoleRepository
+    {
+        private static List<Role> Roles { get; set; } = new List<Role>();
+        private static bool init = false;
+
+        public RoleRepository()
+        {
+            InitializeData();
+        }
+
+        private void InitializeData()
+        {
+            if (init) { return; }
+            else { init = true; }
+
+            //TODO: 
+        }
+
+        public ICollection<Role> GetAll()
+        {
+            return Roles;
+        }
+    }
+}
