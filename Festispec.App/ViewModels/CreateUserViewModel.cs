e65147f976e5b2875771f44b65b9600c311c85f4
Aaron Slots <aaronslots@gmail.com>
2018-11-16 13:39:35 +0100

    passGenerate & Register & CanRegister

--- Festispec.App/ViewModels/CreateUserViewModel.cs
+++ Festispec.App/ViewModels/CreateUserViewModel.cs
@@ -10,10 +10,12 @@ namespace Festispec.App.ViewModels
 {
 	public class CreateUserViewModel : ViewModelBase
 	{
+		private readonly Random _random;
+
 		public string Username { get; set; }
 		public string Password { get; set; }
 		public RoleViewModel Role { get; set; }
-        public ObservableCollection<RoleViewModel> Roles { get; set; }
+		public ObservableCollection<RoleViewModel> Roles { get; set; }
 
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
@@ -26,19 +28,18 @@ namespace Festispec.App.ViewModels
 		public string City { get; set; }
 		public string Street { get; set; }
 		public int HouseNumber { get; set; }
-		private char _houseNumberSuffix;
+		private char? _houseNumberSuffix;
+
 		public string HouseNumberSuffix
 		{
 			get => Convert.ToString(_houseNumberSuffix);
 			set
 			{
+				if (value == null)
+					_houseNumberSuffix = null;
 				try
 				{
 					_houseNumberSuffix = Convert.ToChar(value);
-				}
-				catch (ArgumentNullException e)
-				{
-					Console.WriteLine(e.ToString());
 				}
 				catch (FormatException e)
 				{
@@ -47,27 +48,47 @@ namespace Festispec.App.ViewModels
 			}
 		}
 
-        private IRoleRepository _roleRepo;
-        private IEmployeeRepository _employeeRepo;
-        private IUsersRepository _userRepo;
+		private IRoleRepository _roleRepo;
+		private IEmployeeRepository _employeeRepo;
+		private IUsersRepository _userRepo;
 
-		public RelayCommand RegisterCommand { get; set; }
+		public RelayCommand RegisterCommand { get; private set; }
+		public RelayCommand GeneratePasswordCommand { get; private set; }
 
-        public CreateUserViewModel()
-        {
-            _roleRepo = new RoleTestRepository();
-            _employeeRepo = new EmployeeTestRepository();
-            _userRepo = new UserTestRepository();
-            Roles = new ObservableCollection<RoleViewModel>(_roleRepo.GetAll().Select(r => new RoleViewModel(r)));
-        }
+		public CreateUserViewModel()
+		{
+			_random = new Random();
+			_roleRepo = new RoleTestRepository();
+			_employeeRepo = new EmployeeTestRepository();
+			_userRepo = new UserTestRepository();
+			Roles = new ObservableCollection<RoleViewModel>(_roleRepo.GetAll().Select(r => new RoleViewModel(r)));
+			RegisterCommand = new RelayCommand(Register, CanRegister);
+			GeneratePasswordCommand = new RelayCommand(GeneratePassword);
+		}
+
+		private bool CanRegister()
+		{
+			return !string.IsNullOrWhiteSpace(Username)
+				&& !string.IsNullOrWhiteSpace(Password)
+				&& Role != null
+				&& !string.IsNullOrWhiteSpace(FirstName)
+				&& !string.IsNullOrWhiteSpace(LastName)
+				&& DateOfBirth != null
+				&& !string.IsNullOrWhiteSpace(Email)
+				&& Email.Count(c => c == '@') == 1
+				&& !string.IsNullOrWhiteSpace(Phone)
+				&& !string.IsNullOrWhiteSpace(City)
+				&& !string.IsNullOrWhiteSpace(Street)
+				&& HouseNumber != 0;
+		}
 
 		private void Register()
 		{
-            User user = new User()
-            {
-                Username = Username,
-                Password = Password,
-                Role = Role.ToModel()
+			User user = new User()
+			{
+				Username = Username,
+				Password = Password,
+				Role = Role.ToModel()
 			};
 			Employee employee = new Employee()
 			{
@@ -81,10 +102,36 @@ namespace Festispec.App.ViewModels
 				City = City,
 				Street = Street,
 				HouseNumber = HouseNumber,
-				HouseNumberSuffix = _houseNumberSuffix,
 			};
-            _userRepo.Add(user);
-            _employeeRepo.AddEmployee(employee);
+			if (_houseNumberSuffix != null)
+				employee.HouseNumberSuffix = (char)_houseNumberSuffix;
+			_userRepo.Add(user);
+			_employeeRepo.AddEmployee(employee);
+		}
+
+		private void GeneratePassword()
+		{
+			int amountOfChars = _random.Next(8, 16);
+			char[] chars = new char[amountOfChars];
+			for (int i = 0; i < amountOfChars; i++)
+				chars[i] = RandomChar;
+			Password = new string(chars);
+		}
+
+		private char RandomChar
+		{
+			get
+			{
+				int charNumber = _random.Next(62);
+				char c;
+				if (charNumber < 10)
+					c = char.Parse(charNumber.ToString());
+				else if (charNumber < 36)
+					c = (char)('a' + (charNumber - 10));
+				else
+					c = (char)('A' + (charNumber - 36));
+				return c;
+			}
 		}
 	}
 }
