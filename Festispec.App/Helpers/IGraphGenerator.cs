940b605e3819a0066a4d4f599f436eeb6dd1dc08
Aaron Slots <aaronslots@gmail.com>
2018-11-16 15:25:52 +0100

    graph startup

---
+++ Festispec.App/Helpers/IGraphGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Festispec.App.Helpers
+{
+	public abstract class GraphGeneratorBase
+	{
+		public void CreateGraph()
+		{
+			
+		}
+
+		protected abstract MarshalByRefObject.Component.Control.Chart GenerateChart();
+
+		private void GraphToPNG
+	}
+}
